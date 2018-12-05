9c3f04d5a9c1d6890a50c8e774539c002af98e4a
Camrone Hassell <Camrone.Hassell@gmail.com>
2018-12-05 14:03:36 -0800

    youtube api

---
+++ main.js
@@ -0,0 +1,77 @@
+// Options
+const CLIENT_ID = '302553869549-skv6p63pdfjq3se7f8v72ol8jbfqd176.apps.googleusercontent.com';
+const DISCOVERY_DOCS = [
+    'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
+];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+const authorizeButton = document.getElementById('authorize_button');
+const signoutButton = document.getElementById('signout_button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel_form');
+const channelInput = document.getElementById('channel_input');
+const videoContainer = document.getElementById('video_container');
+
+const defaultChannel = 'Pewdiepie';
+
+
+// Load auth2 library
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+// Init API client library and set up sign in listeners
+function initClient() {
+    gapi.client.init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    })
+    .then(() => {
+        // Listen for sign in state changes
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        // Handle initial sign in state
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+// Update UI sign in state changes
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        content.style.display = 'block';
+        videoContainer.style.display = 'block';
+        getChannel(defaultChannel);
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+        content.style.display = 'none';
+        videoContainer.style.display = 'none';
+    }
+}
+
+// Handle login
+function handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+// Handle logout
+function handleSignoutClick() {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+
+// Display channel data
+function showChannelData(data) {
+    const channelData = document.getElementById('channel_data');
+    channelData.innerHTML = data;
+}
+
+
+// Get channel from API
+function getChannel(channel) {
+    console.log(channel)
+}
